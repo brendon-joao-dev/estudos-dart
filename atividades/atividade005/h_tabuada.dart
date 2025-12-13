1288b84904b085f0addf30301bea3e904f672dc0
Brendon João <brendonjoaodev@gmail.com>
2025-12-13 20:27:06 -0300

    Atividade005: H - Tabuada

---
+++ atividades/atividade005/h_tabuada.dart
@@ -0,0 +1,117 @@
+// H - Faça um programa que receba um número inteiro. O programa deverá imprimir tabuada de multiplicação deste número.
+
+// Importação de módulos:
+import "dart:io";
+
+void main() {
+  System sistema = System(
+    algoritmo: () {
+      Entrada entrada = Entrada();
+      int numero = 0;
+
+      numero = entrada.entrarInt(
+        mensagemEntrada: "Entre com um número inteiro: ",
+        mensagemInvalida: "Por favor entre com um número inteiro!",
+      );
+
+      Calculadora calculadora = Calculadora(n1: numero);
+
+      calculadora.tabuada();
+      print("");
+    },
+  );
+
+  sistema.rodarPrograma();
+}
+
+// Classe System:
+// - Atributos: bool rodando
+// - Métodos: rodarPrograma(), continuarPrograma()
+class System {
+  // Atributos:
+  bool rodando; // Define se o programa deve rodar ou não chamar rodarPrograma()
+  Function algoritmo; // Função que deve ser executada a cada ciclo do programa
+
+  // Construtor:
+  System({required this.algoritmo, this.rodando = false});
+
+  // Método que executa a função algoritmo() de acordo com a flag rodando
+  void rodarPrograma() {
+    this.rodando = true;
+    while (rodando) {
+      algoritmo();
+      continuarPrograma();
+    }
+    this.rodando = false;
+  }
+
+  // Método que pergunta ao usuário se quer continuar o programa, retornando true ou false
+  void continuarPrograma() {
+    String continuar = "";
+    while (true) {
+      stdout.write("Deseja repetir o programa? (s/n): ");
+      continuar = stdin.readLineSync()!.toLowerCase().trim();
+      print("");
+
+      if (continuar == "s") {
+        rodando = true;
+        break;
+      } else if (continuar == "n") {
+        rodando = false;
+        break;
+      } else {
+        print("Por favor digite 's' para sim ou 'n' para não!\n");
+        continue;
+      }
+    }
+  }
+}
+
+// Classe Entrada:
+// - Métodos: entrarInt()
+class Entrada {
+  int entrarInt({
+    String mensagemEntrada = "Entre com um valor: ",
+    String mensagemInvalida = "Por favor entre com algum valor!",
+    bool positivo = false,
+  }) {
+    while (true) {
+      stdout.write(mensagemEntrada);
+      String entrada = stdin.readLineSync() ?? "";
+      print("");
+
+      if (positivo) {
+        if (int.tryParse(entrada) != null && !int.parse(entrada).isNegative) {
+          return int.parse(entrada);
+        } else {
+          print(mensagemInvalida);
+          print("");
+          continue;
+        }
+      } else {
+        if (int.tryParse(entrada) != null) {
+          return int.parse(entrada);
+        } else {
+          print(mensagemInvalida);
+          print("");
+          continue;
+        }
+      }
+    }
+  }
+}
+
+// Classe Calculadora:
+// - Atributos: int n1
+// - Métodos: tabuada()
+class Calculadora {
+  int n1;
+
+  Calculadora({this.n1 = 0});
+
+  void tabuada() {
+    for (int i = 1; i <= 10; i++) {
+      print("$n1 X $i = ${n1 * i}");
+    }
+  }
+}
