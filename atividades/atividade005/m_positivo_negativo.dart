8e06f962016739d218599794005135755bc51884
Brendon João <brendonjoaodev@gmail.com>
2025-12-20 23:18:03 -0300

    Atividade005: M - Verificar se é positivo ou negativo

---
+++ atividades/atividade005/m_positivo_negativo.dart
@@ -0,0 +1,121 @@
+// M - Faça um programa para encontrar o dobro de um número caso ele seja positivo e o seu triplo caso seja negativo, imprimindo o resultado.
+
+// Importação de módulos:
+import "dart:io";
+
+void main() {
+  System sistema = System(
+    algoritmo: () {
+      Entrada entrada = Entrada();
+      int a = 0;
+
+      a = entrada.entrarInt(
+        mensagemEntrada: "Entre com um número inteiro: ",
+        mensagemInvalida: "Por favor entre com um número inteiro!",
+      );
+
+      FiltroModificador filtro = FiltroModificador(a: a);
+
+      filtro.verificarPositivoNegativo();
+    },
+  );
+
+  sistema.rodarPrograma();
+}
+
+// Classe System:
+// - Atributos: bool rodando
+// - Métodos: rodarPrograma(), continuarPrograma()
+class System {
+  // Atributos:
+  bool rodando; // Define se o programa deve rodar ou não chamar rodarPrograma()
+  Function algoritmo; // Função que deve ser executada a cada ciclo do programa
+
+  // Construtor:
+  System({required this.algoritmo, this.rodando = false});
+
+  // Método que executa a função algoritmo() de acordo com a flag rodando
+  void rodarPrograma() {
+    this.rodando = true;
+    while (rodando) {
+      algoritmo();
+      continuarPrograma();
+    }
+    this.rodando = false;
+  }
+
+  // Método que pergunta ao usuário se quer continuar o programa, retornando true ou false
+  void continuarPrograma() {
+    String continuar = "";
+    while (true) {
+      stdout.write("Deseja repetir o programa? (s/n): ");
+      continuar = stdin.readLineSync()!.toLowerCase().trim();
+      print("");
+
+      if (continuar == "s") {
+        rodando = true;
+        break;
+      } else if (continuar == "n") {
+        rodando = false;
+        break;
+      } else {
+        print("Por favor digite 's' para sim ou 'n' para não!\n");
+        continue;
+      }
+    }
+  }
+}
+
+// Classe Entrada:
+// - Métodos: entrarInt()
+class Entrada {
+  int entrarInt({
+    String mensagemEntrada = "Entre com um valor: ",
+    String mensagemInvalida = "Por favor entre com algum valor!",
+    bool positivo = false,
+  }) {
+    while (true) {
+      stdout.write(mensagemEntrada);
+      String entrada = stdin.readLineSync() ?? "";
+      print("");
+
+      if (positivo) {
+        if (int.tryParse(entrada) != null && !int.parse(entrada).isNegative) {
+          return int.parse(entrada);
+        } else {
+          print(mensagemInvalida);
+          print("");
+          continue;
+        }
+      } else {
+        if (int.tryParse(entrada) != null) {
+          return int.parse(entrada);
+        } else {
+          print(mensagemInvalida);
+          print("");
+          continue;
+        }
+      }
+    }
+  }
+}
+
+// Classe FiltroModificador:
+// - Atributos: int a
+// - Métodos: verificarPositivoNegativo()
+class FiltroModificador {
+  int a;
+
+  FiltroModificador({this.a = 0});
+
+  void verificarPositivoNegativo() {
+    if (!a.isNegative) {
+      int c = a * 2;
+      print("O dobro de $a é $c");
+    } else {
+      int c = a * 3;
+      print("O triplo de $a é $c");
+    }
+    print("");
+  }
+}
