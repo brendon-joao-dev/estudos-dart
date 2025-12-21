ae5f75b1c9f2e9d80e98a1e4ba02011cca8d82ad
Brendon João <brendonjoaodev@gmail.com>
2025-12-21 00:25:51 -0300

    Atividade004: Refatoração

--- atividades/atividade004/app_refatorado.dart
+++ atividades/atividade004/app_refatorado.dart
@@ -10,6 +10,210 @@ import "dart:io";
 //        - "subtotal": double - subtotal atual daquele produto
 List<Map<String, dynamic>> carrinho = [];
 
+void main() {
+  while (true) {
+    bool flag = true;
+    exibirMenu();
+
+    int opcao = entrada<int>(
+      mensagemEntrada: "Digite o número da opção desejada: ",
+      mensagemInvalida: "Por favor digite um valor válido como opção!",
+      mensagemNegativo: "Por favor digite um valor válido como opção!",
+      positivo: true,
+    );
+    decoracao();
+
+    if (opcao == 0) {
+      print("Obrigado por usar nosso sistem!");
+      print("Saindo do sistema...");
+      decoracao();
+      break;
+    } else if (opcao == 1) {
+      print("Adição de produtos");
+      decoracao();
+      while (flag) {
+        String nome = "";
+        double preco = 0.0;
+        int quantidade = 0;
+
+        nome = entrada<String>(
+          mensagemEntrada: "Digite o nome do produto: ",
+          mensagemInvalida: "Por favor digite algo como nome do produto!",
+        );
+
+        preco = entrada<double>(
+          mensagemEntrada: "Digite o preço do produto: ",
+          mensagemInvalida: "Por favor digite um número decimal como preço!",
+          mensagemNegativo:
+              "Por favor digite um preço positivo diferente de zero!",
+          positivo: true,
+        );
+
+        quantidade = entrada<int>(
+          mensagemEntrada: "Digite a quantidade do produto: ",
+          mensagemInvalida:
+              "Por favor digite um número inteiro como quantidade!",
+          mensagemNegativo:
+              "Por favor digite uma quantidade positivo diferente de zero!",
+          positivo: true,
+        );
+
+        cadastrarProduto(
+          carrinho: carrinho,
+          nome: nome,
+          preco: preco,
+          quantidade: quantidade,
+        );
+
+        flag = perguntaContinuar("Deseja adicionar outro produto? (s/n): ");
+      }
+    } else if (opcao == 2) {
+      print("Visualização de produtos");
+      print("");
+      print("=" * 70);
+
+      exibirCarrinho();
+    } else if (opcao == 3) {
+      print("Modificação de produtos");
+      print("");
+      print("=" * 70);
+
+      while (flag) {
+        int id = 0;
+        String novoNome;
+        double novoPreco;
+        int novaQuantidade = 0;
+        Map<String, dynamic> produtoAlterando;
+
+        while (true) {
+          exibirCarrinho();
+
+          id = entrada<int>(
+            mensagemEntrada: "Digite o ID do produto que quer alterar: ",
+            mensagemInvalida: "Por favor digite um número inteiro como ID!",
+            mensagemNegativo:
+                "Por favor digite um ID positivo diferente de zero!",
+            positivo: true,
+          );
+
+          if (id > carrinho.length + 1 && id != 0) {
+            print("Por favor digite um ID dentro do intervalo dos produtos!");
+            pressioneContinuar();
+            decoracao();
+          } else {
+            break;
+          }
+        }
+
+        produtoAlterando = carrinho.removeAt(id - 1);
+
+        print(
+          "ID: ${id} Produto: ${produtoAlterando["nome"]} Preço: ${produtoAlterando["preco"]} Quantidade: ${produtoAlterando["quantidade"]} Subtotal: ${produtoAlterando["subtotal"]}",
+        );
+        decoracao();
+
+        novoNome = entrada<String>(
+          mensagemEntrada:
+              "Digite o novo nome do produto ${produtoAlterando["nome"]}: ",
+          mensagemInvalida: "Por favor digite algo como nome do produto!",
+        );
+
+        novoPreco = entrada<double>(
+          mensagemEntrada:
+              "Digite o novo preço do produto ${produtoAlterando["nome"]}: ",
+          mensagemInvalida: "Por favor digite um número decimal como preço!",
+          mensagemNegativo:
+              "Por favor digite um preço positivo diferente de zero!",
+          positivo: true,
+        );
+
+        novaQuantidade = entrada<int>(
+          mensagemEntrada:
+              "Digite a nova quantidade do produto ${produtoAlterando["nome"]}: ",
+          mensagemInvalida:
+              "Por favor digite um número inteiro como quantidade!",
+          mensagemNegativo:
+              "Por favor digite uma quantidade positivo diferente de zero!",
+          positivo: true,
+        );
+
+        modificarProduto(
+          carrinho: carrinho,
+          id: id,
+          novoNome: novoNome,
+          novoPreco: novoPreco,
+          novaQuantidade: novaQuantidade,
+        );
+
+        flag = perguntaContinuar("Deseja modificar outro produto? (s/n): ");
+      }
+    } else if (opcao == 4) {
+      print("Remoção de produtos");
+      print("");
+      print("=" * 70);
+
+      while (flag) {
+        int? id = 0;
+
+        while (true) {
+          exibirCarrinho();
+
+          id = entrada<int>(
+            mensagemEntrada: "Digite o ID do produto que quer remover: ",
+            mensagemInvalida: "Por favor digite um número inteiro como ID!",
+            mensagemNegativo:
+                "Por favor digite um ID positivo diferente de zero!",
+            positivo: true,
+          );
+
+          if (id > carrinho.length + 1 && id != 0) {
+            print("Por favor digite um ID dentro do intervalo dos produtos!");
+            pressioneContinuar();
+            decoracao();
+          } else {
+            break;
+          }
+        }
+
+        removerProduto(carrinho: carrinho, id: id);
+
+        flag = perguntaContinuar("Deseja remover outro produto? (s/n): ");
+      }
+    } else if (opcao == 5) {
+      print("Compra de produtos");
+      print("");
+      print("=" * 70);
+
+      exibirCarrinho();
+
+      Map<String, dynamic> resumo = resumirCarrinho(carrinho: carrinho);
+
+      exibirResumoCarrinho(carrinho: carrinho, resumo: resumo);
+    } else {
+      print("Por favor digite um valor válido como opção!");
+      pressioneContinuar();
+      decoracao();
+      continue;
+    }
+  }
+}
+
+void exibirMenu() {
+  print("=" * 70);
+  print("");
+  print("Bem vindo ao carrinho da loja DartShop!");
+  print("");
+  print("=" * 70);
+  print("");
+  print("0 - Sair do sistema");
+  print("1 - Adicionar produto");
+  print("2 - Visualizar carrinho");
+  print("3 - Modificar produtos");
+  print("4 - Remover produtos");
+  print("5 - Comprar produtos");
+  print("");
+}
+
 void decoracao() {
   print("");
   print("=" * 70);
@@ -162,7 +366,7 @@ void exibirCarrinho({List<Map<String, dynamic>>? carrinho}) {
 }
 
 bool modificarProduto({
-  List<Map<String, dynamic>>? carrinho ,
+  List<Map<String, dynamic>>? carrinho,
   int id = 0,
   String? novoNome,
   double? novoPreco,
@@ -234,9 +438,7 @@ bool removerProduto({List<Map<String, dynamic>>? carrinho, int id = 0}) {
   }
 }
 
-Map<String, dynamic> resumirCarrinho({
-  List<Map<String, dynamic>>? carrinho,
-}) {
+Map<String, dynamic> resumirCarrinho({List<Map<String, dynamic>>? carrinho}) {
   carrinho = carrinho ?? [];
   int totalUnidades = 0;
   int totalItens = 0;
@@ -300,7 +502,10 @@ Map<String, dynamic> resumirCarrinho({
   };
 }
 
-void exibirResumoCarrinho({List<Map<String, dynamic>>? carrinho, Map<String, dynamic>? resumo}) {
+void exibirResumoCarrinho({
+  List<Map<String, dynamic>>? carrinho,
+  Map<String, dynamic>? resumo,
+}) {
   carrinho = carrinho ?? [];
   resumo = resumo ?? {};
 
@@ -317,8 +522,12 @@ void exibirResumoCarrinho({List<Map<String, dynamic>>? carrinho, Map<String, dyn
     print(
       "O produto mais caro é ${resumo["produto_mais_caro"]["nome"]} custando ${resumo["produto_mais_caro"]["preco"]} por unidade",
     );
-    print("A média dos preços dos produtos é: ${resumo["media_precos_simples"]}");
-    print("A média dos subtotais dos produtos é: ${resumo["media_precos_ponderada"]}");
+    print(
+      "A média dos preços dos produtos é: ${resumo["media_precos_simples"]}",
+    );
+    print(
+      "A média dos subtotais dos produtos é: ${resumo["media_precos_ponderada"]}",
+    );
     print("");
 
     pressioneContinuar();
